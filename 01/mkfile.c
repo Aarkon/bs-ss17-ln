e276517a97987172f0a7bfd223296be4afcdbd9a
Fluentifer <Jakob.Ledig@haw-hamburg.de>
2017-04-05 18:20:43 +0200

    Doku geupdatet, C-Code fast fertig (nur Datei anlegen fehlt noch)

--- 01/mkfile.c
+++ 01/mkfile.c
@@ -1,5 +1,24 @@
-#include <stdio.h>
+/* authors: Jakob Ledig & Florian Nehmer */
+/* Praktikum Betriebssysteme Sommersemester 2017 */
+/* Praktiumsgruppe 4 */
+/* Aufgabe 1 */
 
-int main (){
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char *argv[]){
+  char input[30];
+  
+  if (argc == 2){
+    printf("argument was: %s\n", argv[1]);
+    strcpy(input, argv[1]);
+    if (strlen(argv[1]) > 30) {
+      printf("Argument too long!");
+    }
+  }
+  else {
+    scanf("Enter the file name: %s", input);
+    
+  }
   return 0;
 }
