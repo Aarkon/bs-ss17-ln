855fe17fa07b2ed4baf8ecdab3b9c61deef7db08
Fluentifer <Jakob.Ledig@haw-hamburg.de>
2017-04-05 21:58:13 +0200

    Läuft jetzt auch, hat mir keine Ruhe gelassen.

--- 01/try_host.sh
+++ 01/try_host.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-function pinghost() {
-  result=$(ping -c 1 "$1")
-  if [[ -n $result ]]; then
-    echo "$1 OK"
-  else
-    echo "$1 FAILED"
-  fi
-  echo "$result"
-}
-
 #--------------------------------------main------------------------------------#
 #
 
 if [ $1 == "-h" ]; then
+    echo "This cli-tool is an interface to the ping program."
     echo "Parameter list:"
     echo "-h: Display this help message"
-    echo "-s \$number \$host: ping a host with a given name at a certain interval"
+    echo "-s \$number \$host: ping host at a certain interval"
+    echo ""
+    echo "Example usage:"
+    echo "try_host.sh -s 5 www.google.de"
+
   elif [ $1 == "-s" ]; then
       echo "Pinging in intervals..."
-      host=$2
-      interval=$3
-      while [[ true ]]; do
-        pinghost $host
+      interval=$2
+      host=$3
+      while [ true ]; do
+        result=$(ping -c 1 "$host")
+        if [ $? == 0 ]; then
+          echo "$host OK"
+        else
+          echo "$host FAILED"
+        fi
+        echo "Sleeping for $interval seconds..."
         sleep $interval
+        echo ""
       done
 fi
