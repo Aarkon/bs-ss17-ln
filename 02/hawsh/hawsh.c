b53c3b7c5172850706f30057aa0e496647fd1be1
Fluentifer <Jakob.Ledig@haw-hamburg.de>
2017-05-03 09:32:06 +0200

    Struktur für C-Teil der Aufgabe angelegt

---
+++ 02/hawsh/hawsh.c
@@ -0,0 +1,18 @@
+/*
+ * A C-Program to implement/mimic a simple shell.
+ *
+ * Jakob Ledig & Florian Nehmer, 3.5.2017
+ * Praktikum Betriebssysteme SS2017 Gruppe 4
+ *
+ * Best compile using the following command:
+ * cd bs-ss17-ln/02/hawsh
+ * gcc -g -Wall -Wextra -pedantic-errors hawsh.c -o build/hawsh
+ */
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]){
+    printf("Hello World!\n");
+}
