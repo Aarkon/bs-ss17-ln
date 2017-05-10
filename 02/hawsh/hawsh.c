bcb05b770ba06f0d6583528bfeb558b5b11bd398
florian <florian@FN>
2017-05-10 13:30:21 +0200

    hawshell v1.0 fertig

--- 02/hawsh/hawsh.c
+++ 02/hawsh/hawsh.c
@@ -15,12 +15,11 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <wait.h>
 
 
 /**
- * Print the Prompt String:
- * "usint read_command(char);ername@cwd >"
- *
+ * Prints the Prompt String
  */
 void type_prompt() {
     char cwd[256];
@@ -30,9 +29,10 @@ void type_prompt() {
 
 
 /**
+ * Reads the keyboard input Stream and saves the input into param command
  *
- * @param command
- * @return
+ * @param command: Holds the user input
+ * @return 0 = a command without &, 1 = a command with &
  */
 int read_command(char *command) {
     fgets(command, 128, stdin);
@@ -45,6 +45,23 @@ int read_command(char *command) {
     }
 }
 
+/**
+ * Prints help text
+ */
+void usage() {
+    printf("hawsh is a shell, which can execute the following built-in commands:\n");
+    printf("help - shows this help message\n");
+    printf("version - shows the current version of hawsh\n");
+    printf("/[pathname] - change the current working directory to pathname\n");
+    printf("quit - closes hawsh");
+}
+
+/**
+ * Prints version info
+ */
+void version() {
+    printf("1.0");
+}
 
 
 /**
@@ -55,23 +72,39 @@ int read_command(char *command) {
  */
 int main(int argc, char **argv[]) {
     int PIDstatus;
+
     int command_in_background = 0;
+    int status = 0;
     char command[128];
 
     while (true) {
         type_prompt();
-        read_command(&command);
+        command_in_background=read_command(&command);
 
         if (strcmp(command, "help") == 0) {
-            printf("Execute help function\n");
+            usage();
         } else if (strcmp(command, "version") == 0) {
-            printf("Execute version function\n");
+            version();
         } else if (strcmp(command, "quit") == 0) {
             exit(EXIT_SUCCESS);
         } else if (strncmp(command, "/", 1) == 0) {
-            printf("Execute cd function\n");
+            if(chdir(command) == -1)
+            {
+                printf("failed to change directory");
+            }
         } else {
-            printf("Execute execlp and check if run in background");
+            int PIDstatus = fork();
+            if (PIDstatus < 0){
+                printf("Unable to fork");
+                continue;
+            }
+            if(PIDstatus > 0){
+                if(command_in_background){
+                    waitpid(PIDstatus, &status, 0);
+                }
+            } else {
+                execlp(command, command, NULL);
+            }
         }
     }
 }
