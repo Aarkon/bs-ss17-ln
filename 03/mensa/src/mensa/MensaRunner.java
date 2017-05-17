f86bda157665ef9b047ebb9fc7e7656c491231ad
florian <florian@FN>
2017-05-17 17:58:29 +0200

    edited Mensa

--- 03/mensa/src/mensa/MensaRunner.java
+++ 03/mensa/src/mensa/MensaRunner.java
@@ -4,7 +4,9 @@ package mensa;
  * Created by jakob on 16.05.17.
  */
 public class MensaRunner {
-    public static void main(String[] args) {
-        // Here be logic
-    }
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+	
 }
